6a151f142ad370cba90b0abb79309ea99b439c00
Hannes Olszewski <kontakt@hannes-olszewski.dev>
2023-04-19 13:30:42 +0200

    feat: add global spacing values

---
+++ src/styles/spacing.ts
@@ -0,0 +1,15 @@
+/**
+ * Consistent spacing values for the app, e.g. for margins or paddings.
+ */
+const spacing = {
+  none: 0,
+  xxs: 1,
+  xs: 2,
+  s: 4,
+  m: 16,
+  l: 32,
+  xl: 48,
+  xxl: 64,
+};
+
+export default spacing;
