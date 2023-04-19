e6294ef58ca051428ad851078a6d6a1057c80aa6
Hannes Olszewski <kontakt@hannes-olszewski.dev>
2023-04-19 13:37:52 +0200

    feat: store active mower connection in global context

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import LayoutAndNavigation from './components/layout/LayoutAndNavigation';
 import useIsInDarkMode from './hooks/useIsInDarkMode';
 import './i18n.config';
+import {
+  ActiveMowerConnectionContext,
+  MowerConnection,
+} from './hooks/useActiveMowerConnection';
 
 /**
  * The Mow-E Mobile app. Renders the complete application.
  */
 function App(): JSX.Element {
+  const [activeMowerConnection, setActiveMowerConnection] =
+    useState<MowerConnection | null>(null);
   const isDarkMode = useIsInDarkMode();
 
   return (
     <NavigationContainer>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <LayoutAndNavigation />
+      <ActiveMowerConnectionContext.Provider
+        value={{
+          activeConnection: activeMowerConnection,
+          setActiveConnection: setActiveMowerConnection,
+        }}>
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+        <LayoutAndNavigation />
+      </ActiveMowerConnectionContext.Provider>
     </NavigationContainer>
   );
 }
---
+++ src/hooks/useActiveMowerConnection.ts
@@ -0,0 +1,47 @@
+import {createContext, useContext} from 'react';
+
+/**
+ * An (available) connection to a mower. Doesn't have to be an active connection.
+ */
+export interface MowerConnection {
+  /**
+   * A unique id representing the mower.
+   */
+  id: string | number;
+  /**
+   * The name of the mower.
+   */
+  name: string;
+}
+
+/**
+ * The properties saved in the `ActiveMowerConnectionContext`.
+ */
+interface ActiveMowerConnectionContextType {
+  /**
+   * The (currently) active connection to a mower, or null if no active connection.
+   */
+  activeConnection: MowerConnection | null;
+  /**
+   * Setter for the active connection.
+   *
+   * @param connection the new connection to set active.
+   */
+  setActiveConnection: (connection: MowerConnection | null) => void;
+}
+
+/**
+ * Context that stores the (currently) active connection to a mower and a setter to change it.
+ */
+export const ActiveMowerConnectionContext =
+  createContext<ActiveMowerConnectionContextType>({
+    activeConnection: null,
+    setActiveConnection: () => {},
+  });
+
+/**
+ * Returns the (currently) active connection to a mower and a setter to change it.
+ */
+const useActiveMowerConnection = () => useContext(ActiveMowerConnectionContext);
+
+export default useActiveMowerConnection;
