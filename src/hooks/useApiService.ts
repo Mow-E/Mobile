f1f319661b478646cfacb77f05e91e1e3b5bb9a4
Hannes Olszewski <kontakt@hannes-olszewski.dev>
2023-05-12 22:57:49 +0200

    refactor: move `ApiCalls` to service+hook

---
+++ src/hooks/useApiService.ts
@@ -0,0 +1,60 @@
+import {useCallback} from 'react';
+import {fetchWithAuthorization, LOGIN_URL} from '../services/api';
+
+type LoginFetchResult =
+  | {
+      status: 'successful';
+      token: string;
+    }
+  | {
+      status: 'error';
+      message: string;
+    };
+
+/**
+ * Provides functions to use the API.
+ */
+function useApiService() {
+  // The current user can be used here later on to check that a user/token
+  // exists before making api calls that need authorization
+  // const currentUser = useCurrentUser();
+
+  /**
+   * Tries to perform a login with the provided credentials.
+   * Resolves to the token on success, or an empty string on failure.
+   */
+  const login = useCallback<
+    (username: string, password: string) => Promise<string>
+  >(async (username, password) => {
+    try {
+      const response = await fetchWithAuthorization(
+        LOGIN_URL,
+        {
+          username: username,
+          password: password,
+          // expiresInMins: 60, // optional
+        },
+        'POST',
+        undefined,
+      );
+      const data: LoginFetchResult = await response.json();
+      console.log(data);
+
+      if (data.status === 'successful') {
+        return data.token;
+      }
+
+      throw new Error(`login unsuccessful: ${data.message}`);
+    } catch (error) {
+      console.error(error);
+      // TODO: better error handling needed here
+      return '';
+    }
+  }, []);
+
+  return {
+    login,
+  };
+}
+
+export default useApiService;
---
+++ src/services/api.ts
@@ -0,0 +1,33 @@
+/** The base url of the API */
+const BASE_URL = 'http://49.12.44.36:8080';
+/** The url to the login endpoint */
+export const LOGIN_URL = `${BASE_URL}/auth/login`;
+
+/**
+ * Fetches the given url with an added authorization token (if given).
+ */
+export async function fetchWithAuthorization(
+  url: string,
+  body: object = {},
+  method: 'GET' | 'POST' = 'GET',
+  authorizationToken?: string,
+): Promise<Response> {
+  if (authorizationToken === undefined) {
+    return fetch(url, {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+  }
+
+  return fetch(url, {
+    method,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${authorizationToken}`,
+    },
+    body: JSON.stringify(body),
+  });
+}
