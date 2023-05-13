53d6960e1da414ad530b6ff1bb1ff919f1a590b6
Hannes Olszewski <kontakt@hannes-olszewski.dev>
2023-05-13 14:35:13 +0200

    feat: add service to send commands to the active mower

--- src/hooks/useBluetoothService.ts
+++ src/hooks/useBluetoothService.ts
@@ -1,6 +1,47 @@
 import {useCallback} from 'react';
 import useActiveMowerConnection from './useActiveMowerConnection';
-import {disconnect as disconnectMower} from '../services/bluetooth';
+import {
+  disconnect as disconnectMower,
+  sendMessage,
+} from '../services/bluetooth';
+
+/**
+ * A command that can be sent to the current connected mower.
+ */
+export enum MowerCommand {
+  /**
+   * Starts the mower (when in automatic mode).
+   */
+  Start = '?',
+  /**
+   * Stops the mower.
+   */
+  Stop = 'S',
+  /**
+   * Lets the mower continuously move forward until stopped (if in manual mode).
+   */
+  MoveForward = 'F',
+  /**
+   * Lets the mower continuously move backward until stopped (if in manual mode).
+   */
+  MoveBackward = 'B',
+  /**
+   * Lets the mower continuously move right until stopped (if in manual mode).
+   */
+  MoveRight = 'R',
+  /**
+   * Lets the mower continuously move left until stopped (if in manual mode).
+   */
+  MoveLeft = 'L',
+  /**
+   * Changes the current mode of the mower to automatic.
+   */
+  ChangeModeToAutomatic = 'A',
+  /**
+   * Changes the current mode of the mower to manual.
+   */
+  ChangeModeToManual = 'M',
+}
 
 /**
  * Provides service functions for interacting with the active mower connection
@@ -21,7 +62,25 @@ function useBluetoothService() {
     console.warn('Cannot disconnect active connection since there is none');
   }, [activeConnection, setActiveConnection]);
 
-  return {disconnect};
+  /**
+   * Sends a command to the current active mower, or throws an error if no active connection.
+   *
+   * @param command MowerCommand the command to send.
+   */
+  const sendCommand = useCallback<(command: MowerCommand) => Promise<void>>(
+    async command => {
+      if (activeConnection === null) {
+        throw new Error(
+          `Cannot send command ${command} because there is no active connection`,
+        );
+      }
+
+      await sendMessage(command, activeConnection);
+    },
+    [activeConnection],
+  );
+
+  return {disconnect, sendCommand};
 }
 
 export default useBluetoothService;
--- src/services/bluetooth.ts
+++ src/services/bluetooth.ts
@@ -1,5 +1,9 @@
 import {MowerConnection} from '../hooks/useActiveMowerConnection';
 import BleManager from 'react-native-ble-manager';
+import {Buffer} from 'buffer';
+
+/** The default ending sequence for messages to mowers. */
+export const DEFAULT_ENDING_SEQUENCE = '*';
 
 export async function startBluetoothService(): Promise<void> {
   await BleManager.start();
@@ -9,16 +13,141 @@ export async function startBluetoothService(): Promise<void> {
 export async function disconnect(
   mowerConnection: MowerConnection,
 ): Promise<MowerConnection> {
-  if (mowerConnection.bluetoothInfos === undefined) {
-    throw new Error(
-      `Cannot disconnect from mower ${mowerConnection.id} because there are no bluetooth infos about it`,
-    );
-  }
+  requireBluetoothInfosInConnection(
+    mowerConnection,
+    `Cannot disconnect from mower ${mowerConnection.id} because there are no bluetooth infos about it`,
+  );
 
-  await BleManager.disconnect(mowerConnection.bluetoothInfos.id);
+  await BleManager.disconnect(mowerConnection.bluetoothInfos!.id);
   console.debug(
-    `[ble] disconnected from ${mowerConnection.id} with bluetooth id ${mowerConnection.bluetoothInfos.id}`,
+    `[ble] disconnected from ${mowerConnection.id} with bluetooth id ${
+      mowerConnection.bluetoothInfos!.id
+    }`,
   );
 
   return mowerConnection;
+}
+
+/**
+ * Options to adjust the message sent to a mower.
+ */
+interface MessageOptions {
+  /**
+   * Character that signals the end of a message.
+   * Defaults to `DEFAULT_ENDING_SEQUENCE`.
+   */
+  endingSequence?: string;
+  /**
+   * The bluetooth service id to send the message to.
+   * Defaults to the first service id found for the bluetooth connection.
+   */
+  serviceId?: string;
+  /**
+   * The bluetooth characteristic id to send the message to.
+   * Defaults to the first characteristic id found for the bluetooth connection.
+   */
+  characteristicId?: string;
+}
+
+/**
+ * Sends a message to the given mower connection.
+ *
+ * @param message string the message to send.
+ * @param mowerConnection MowerConnection the connection to send the message to. Needs to have its bluetooth infos specified.
+ * @param options MessageOptions optional options to adjust the message sending.
+ */
+export async function sendMessage(
+  message: string,
+  mowerConnection: MowerConnection,
+  options: MessageOptions = {},
+): Promise<MowerConnection> {
+  const {
+    endingSequence = DEFAULT_ENDING_SEQUENCE,
+    serviceId,
+    characteristicId,
+  } = options;
+  requireBluetoothInfosInConnection(
+    mowerConnection,
+    `Cannot send message to mower ${mowerConnection.id} because there are no bluetooth infos about it`,
+  );
+
+  const deviceConnectionInfos = await getDeviceConnectionInfos(mowerConnection);
+
+  const serviceIdToUse = serviceId ?? deviceConnectionInfos.serviceIds[0];
+  if (!deviceConnectionInfos.serviceIds.includes(serviceIdToUse)) {
+    console.error(`[ble] service ${serviceIdToUse} not found`);
+    throw new Error(`Service ${serviceIdToUse} not found`);
+  }
+
+  const characteristicIdToUse =
+    characteristicId ?? deviceConnectionInfos.characteristicIds[0];
+  if (
+    !deviceConnectionInfos.characteristicIds.includes(characteristicIdToUse)
+  ) {
+    console.error(`[ble] characteristic ${characteristicIdToUse} not found`);
+    throw new Error(`Characteristic ${characteristicIdToUse} not found`);
+  }
+
+  const data = Buffer.from(message + endingSequence);
+
+  await BleManager.write(
+    mowerConnection.bluetoothInfos!.id,
+    serviceIdToUse,
+    characteristicIdToUse,
+    data.toJSON().data,
+  );
+  console.debug(
+    `[ble] sent message to mower ${mowerConnection.id} with bluetooth id ${
+      mowerConnection.bluetoothInfos!.id
+    }`,
+    data.toString(),
+  );
+
+  return mowerConnection;
+}
+
+export async function getDeviceConnectionInfos(
+  mowerConnection: MowerConnection,
+): Promise<{serviceIds: string[]; characteristicIds: string[]}> {
+  requireBluetoothInfosInConnection(
+    mowerConnection,
+    `Cannot get device infos of mower ${mowerConnection.id} because there are no bluetooth infos about it`,
+  );
+
+  const peripheralInfo = await BleManager.retrieveServices(
+    mowerConnection.bluetoothInfos!.id,
+  );
+  console.debug(
+    `[ble] read info from mower ${mowerConnection.id} with bluetooth id ${
+      mowerConnection.bluetoothInfos!.id
+    }`,
+    peripheralInfo,
+  );
+
+  const serviceIds =
+    peripheralInfo.services?.map(service => service.uuid) ?? [];
+  const characteristicIds =
+    peripheralInfo.characteristics?.map(
+      characteristic => characteristic.characteristic,
+    ) ?? [];
+
+  return {serviceIds, characteristicIds};
+}
+
+/**
+ * Checks that a given mower connection as bluetooth infos specified.
+ * Throws an error if not.
+ *
+ * @param connection MowerConnection the connection to check.
+ * @param errorMessage string|undefined optional message for the error thrown.
+ */
+function requireBluetoothInfosInConnection(
+  connection: MowerConnection,
+  errorMessage?: string,
+): void {
+  if (connection.bluetoothInfos === undefined) {
+    throw new Error(
+      errorMessage ?? `Mower ${connection.id} has no bluetooth infos about it`,
+    );
+  }
 }
