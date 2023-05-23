24b9a38d6d1e7d29233a046ae3b70ae1270bcad8
Hannes Olszewski <kontakt@hannes-olszewski.dev>
2023-05-23 17:59:57 +0200

    feat: add api function for loading mower history events

--- src/hooks/useApiService.ts
+++ src/hooks/useApiService.ts
@@ -1,8 +1,32 @@
 import {useCallback} from 'react';
-import {fetchWithAuthorization, LOGIN_URL, REGISTER_URL} from '../services/api';
+import {
+  fetchWithAuthorization,
+  LOGIN_URL,
+  MOWER_HISTORY_URL,
+  REGISTER_URL,
+} from '../services/api';
+import useCurrentUser from './useCurrentUser';
 import useErrorState from './useErrorState';
 
 export const NO_TOKEN = '';
+
+/**
+ * A state a mower can have had in a mower history event.
+ */
+export enum MowerHistoryEventMowerState {
+  /** The mower has started at the event. */
+  Start = 0,
+  /** The mower was working at the event. */
+  Working = 1,
+  /** The mower has stopped at the event. */
+  Stop = 2,
+  /** The mower has had an error at the event. */
+  Error = 3,
+  /** The mower has avoided a collision at the event. */
+  Collision = 4,
+  /** The mower has hit a border at the event. */
+  Border = 5,
+}
 
 type LoginFetchResult =
   | {
@@ -25,12 +49,66 @@ type RegisterFetchResult =
     };
 
 /**
+ * An event that has been reported by a mower.
+ * It always contains its position and state, with some other data.
+ */
+type MowerHistoryEvent = {
+  /**
+   * The unique id of the event.
+   */
+  id: number;
+  /**
+   * The id of the mowing session the event occured in.
+   */
+  sessionId: number;
+  /**
+   * The id of the mower that reported the event.
+   */
+  mowerId: string;
+  /**
+   * The x position the mower was at when the event occured.
+   */
+  x: number;
+  /**
+   * The y position the mower was at when the event occured.
+   */
+  y: number;
+  /**
+   * The angle the mower had when the event occured.
+   * Its a number in degrees (0.0 .. 360.0).
+   */
+  z: number;
+  /**
+   * The time the event occured at (as a timestamp).
+   */
+  time: number;
+  /**
+   * The state the mower had when the event occured.
+   */
+  state: MowerHistoryEventMowerState;
+  /**
+   * Optional id of an image that is associated with the event, e.g. an image the mower took when avoiding a collision.
+   */
+  imageId: string | null;
+  /**
+   * Extra data for the event.
+   * If the event contains an image (imageId !== null), this field contains classification results for the image, or an empty string otherwise.
+   *
+   * The classification results are a map of label (string) to its probability.
+   * E.g.: "{Foo=0.92141,Bar=0.125151,Foo Bar=0.2718283}"
+   */
+  extra: string;
+};
+
+type MowerHistoryFetchResult =
+  | MowerHistoryEvent[]
+  | {status: 'error'; message: string};
+
+/**
  * Provides functions to use the API.
  */
 function useApiService() {
-  // The current user can be used here later on to check that a user/token
-  // exists before making api calls that need authorization
-  // const currentUser = useCurrentUser();
+  const {currentUser} = useCurrentUser();
   const {setErrorState} = useErrorState();
 
   /**
@@ -53,18 +131,18 @@ function useApiService() {
           undefined,
         );
         const data: LoginFetchResult = await response.json();
-        console.log(data);
 
         if (data.status === 'successful') {
+          console.debug('[api] logged in successfully');
           return data.token;
         }
 
-        console.error(data.message);
+        console.error('[api] login unsuccessful', data.message);
         setErrorState(data.message);
 
         return NO_TOKEN;
       } catch (error) {
-        console.error(error);
+        console.error('[api] login unsuccessful', error);
 
         if (error instanceof Error) {
           setErrorState(error.message);
@@ -92,19 +170,18 @@ function useApiService() {
           undefined,
         );
         const data: RegisterFetchResult = await response.json();
-        console.log(data);
 
         if (data.status === 'successful') {
-          console.log('Successfull registration');
+          console.debug('[api] registered in successfully');
           return data.token;
         }
 
-        console.error(data.reason);
+        console.error('[api] registration unsuccessful', data.reason);
         setErrorState(data.reason);
 
         return NO_TOKEN;
       } catch (error) {
-        console.error(error);
+        console.error('[api] registration unsuccessful', error);
 
         if (error instanceof Error) {
           setErrorState(error.message);
@@ -116,9 +193,73 @@ function useApiService() {
     [setErrorState],
   );
 
+  const getMowerHistory = useCallback<
+    (
+      includeAllSessions?: boolean,
+      specificSessionId?: number,
+    ) => Promise<MowerHistoryEvent[]>
+  >(
+    async (includeAllSessions = true, specificSessionId) => {
+      if (currentUser === null) {
+        throw new Error('Not logged in');
+      }
+
+      const searchParams = new URLSearchParams();
+
+      if (includeAllSessions) {
+        searchParams.append('allSessions', 'true');
+      }
+
+      if (specificSessionId !== undefined) {
+        searchParams.append('sessionId', specificSessionId.toString());
+      }
+
+      try {
+        const response = await fetchWithAuthorization(
+          `${MOWER_HISTORY_URL}?${searchParams}`,
+          {},
+          'GET',
+          currentUser.authorizationToken,
+        );
+        const data: MowerHistoryFetchResult = await response.json();
+
+        const isError = 'status' in data && data.status === 'error';
+
+        if (!isError) {
+          console.log(
+            '[api] loaded mower history event data successfully',
+            data,
+          );
+          return data as MowerHistoryEvent[];
+        } else {
+          console.error(
+            '[api] loading of mower history event data unsuccessful',
+            data.message,
+          );
+          setErrorState(data.message);
+        }
+
+        return [];
+      } catch (error) {
+        console.error(
+          '[api] loading of mower history event data unsuccessful',
+          error,
+        );
+
+        if (error instanceof Error) {
+          setErrorState(error.message);
+        }
+
+        return [];
+      }
+    },
+    [setErrorState, currentUser],
+  );
+
   return {
     login,
     register,
+    getMowerHistory,
   };
 }
 
