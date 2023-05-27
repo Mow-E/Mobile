61fa83adc993700881bc8689681a003bd5468ac8
Hannes Olszewski <kontakt@hannes-olszewski.dev>
2023-05-27 19:38:36 +0200

    fix: add empty state to images list and make pull-to-refresh easier to use

--- src/pages/ImageHistoryPage.tsx
+++ src/pages/ImageHistoryPage.tsx
@@ -1,4 +1,4 @@
-import {FlatList, StyleSheet, View} from 'react-native';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
 import React, {useCallback, useEffect, useMemo, useState} from 'react';
 import useApiService from '../hooks/useApiService';
 import spacing from '../styles/spacing';
@@ -13,6 +13,10 @@ import MowerHistoryEventImageListItem from '../components/image-history/MowerHis
 import useActiveMowerConnection from '../hooks/useActiveMowerConnection';
 import useMowerHistoryEvents from '../hooks/useMowerHistoryEvents';
 import useMowingSessionsToShowInHistory from '../hooks/useMowingSessionsToShowInHistory';
+import useStyles from '../hooks/useStyles';
+import CameraIcon from '../assets/icons/CameraIcon';
+import useIsInDarkMode from '../hooks/useIsInDarkMode';
+import {useTranslation} from 'react-i18next';
 
 /**
  * The page that shows the history of images that the mower took.
@@ -23,6 +27,9 @@ function ImageHistoryPage(): JSX.Element {
   const {activeConnection} = useActiveMowerConnection();
   const {sessionsToShow} = useMowingSessionsToShowInHistory();
   const apiService = useApiService();
+  const styles = useStyles();
+  const isInDarkMode = useIsInDarkMode();
+  const {t} = useTranslation();
 
   const fetchAndUpdateEvents = useCallback(async () => {
     setLoading(true);
@@ -69,6 +76,17 @@ function ImageHistoryPage(): JSX.Element {
         refreshing={loading}
         renderItem={({item}) => <MowerHistoryEventImageListItem item={item} />}
       />
+      {items.length === 0 && (
+        <View
+          style={[styles.centeredContent, componentStyles.emptyStateContainer]}>
+          <Text style={[styles.textNormal, componentStyles.emptyStateText]}>
+            {t('routes.imageHistory.emptyListText')}
+          </Text>
+          <View style={componentStyles.emptyStateIcon}>
+            <CameraIcon size={225} darkModeInverted={isInDarkMode} />
+          </View>
+        </View>
+      )}
     </View>
   );
 }
@@ -79,8 +97,19 @@ function ImageHistoryPage(): JSX.Element {
 const componentStyles = StyleSheet.create({
   pageContainer: {
     gap: spacing.l,
+    height: '100%',
   },
   list: {padding: spacing.l},
+  emptyStateContainer: {
+    position: 'absolute',
+    height: '100%',
+    width: '100%',
+    paddingHorizontal: spacing.l,
+    // Make sure the state is rendered under the list so the pull-to-refresh of the list still works
+    zIndex: -1,
+  },
+  emptyStateText: {paddingBottom: spacing.xxl},
+  emptyStateIcon: {position: 'absolute', bottom: spacing.xxl},
 });
 
 export default ImageHistoryPage;
